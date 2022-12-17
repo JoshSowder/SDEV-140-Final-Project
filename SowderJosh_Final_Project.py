960b4635bfbce7c4d36297b785920caa0616dc19
JoshSowder <74932661+JoshSowder@users.noreply.github.com>
2022-12-16 23:45:25 -0500

    Update SowderJosh_Final_Project.py

--- SowderJosh_Final_Project.py
+++ SowderJosh_Final_Project.py
@@ -21,8 +21,8 @@ class PCT:
 
         self.v1 = StringVar()
         self.search = Entry(frame, width = 125, textvariable = self.v1).grid(row = 1, column = 1, columnspan = 15, padx = 10, pady = 10)
-        self.searchBt = Button(frame, text = "Search", command = self.displayItems(self.webscraper(self.v1.get()))).grid(row = 1, column = 16, pady = 10, ipadx = 4, ipady = 2)
-        self.display = Listbox(frame, width = 175, height = 50).grid(row = 2, column = 1, rowspan = 25, columnspan = 25, padx = 10, pady = 10)
+        self.searchBt = Button(frame, text = "Search", command = self.displayItems(webscraper(self.v1.get()))).grid(row = 1, column = 16, pady = 10, ipadx = 4, ipady = 2)
+        self.display = PanedWindow(frame, width = 175, height = 50).grid(row = 2, column = 1, rowspan = 25, columnspan = 25, padx = 10, pady = 10)
         self.displayWindow = PanedWindow(self.display, orient = VERTICAL)
         self.item1 = Canvas(self.displayWindow, yscrollcommand = TRUE)
         self.scroll1 = Scrollbar(self.item1, orient = VERTICAL).set(0, 1)
@@ -33,65 +33,66 @@ class PCT:
         self.displayWindow.add(self.item1)
         self.displayWindow.add(self.item2)
         self.displayWindow.add(self.item3)
-        self.comparator = Listbox(frame, width = 60, height = 50).grid(row = 2, column = 26, rowspan = 25, columnspan = 8, padx = 10, pady = 10)
+        self.comparator = Listbox(frame, width = 60, height = 50, yscrollcommand = VERTICAL).grid(row = 2, column = 26, rowspan = 25, columnspan = 8, padx = 10, pady = 10)
+        self.scroll4 = Scrollbar(self.comparator, orient = VERTICAL).set(0, 1)
 
         window.mainloop()
        
-    def webscraper(self, searchitem):
-        count = 0
-        #Adapted from example code - edureka.com/blog/web-scraping-with-python/
-        #Credit: Omkar S Hiremath
-        driver = webdriver.Chrome("D:\Program Files (x86)\ChromeDriver\chromedriver")
-        products=[] #List to store name of the product
-        prices=[] #List to store price of the product
-        ratings=[] #List to store rating of the product
-        while count <= 3:
-            if count == 1:
-                driver.get('<a href="https://www.amazon.com/s?k="' + searchitem + '</a>')
-                content = driver.page_source
-                soup = BeautifulSoup(content)
-                for a in soup.findAll(a, href = True, attrs = {'class':'s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16'}):
-                    name=a.find('div', attrs = {'class':'a-section a-spacing-none puis-padding-right-small s-title-instructions-style'})
-                    price=a.find('div', attrs = {'class':'a-section a-spacing-none a-spacing-top-micro s-price-instructions-style'})
-                    rating=a.find('div', attrs = {'class':'a-section a-spacing-none a spacing-top-micro'})
-                    products.append(name.text)
-                    prices.append(price.text)
-                    ratings.append(rating.text)
-                self.df1 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
-                self.df1.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
-            elif count == 2:
-                driver.get('<a href="https://www.walmart.com/search?q="' + searchitem + '</a>')
-                content = driver.page_source
-                soup = BeautifulSoup(content)
-                for a in soup.findAll(searchitem, href = True, attrs = {'class':'mb1 ph1 pa0-xl bb b--near-white w-25'}):
-                    name=a.find('div', attrs = {'class':'w_V_DM'})
-                    price=a.find('div', attrs = {'class':'flex flex-wrap justify-start items-center lh-title mb2 mb1-m'})
-                    rating=a.find('div', attrs = {'class':'flex items-center mt2'})
-                    products.append(name.text)
-                    prices.append(price.text)
-                    ratings.append(rating.text)
-                self.df2 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
-                self.df2.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
-            else:
-                driver.get('<a href="https://www.target.com/s?searchTerm="' + searchitem + '</a>')
-                content = driver.page_source
-                soup = BeautifulSoup(content)
-                for a in soup.findAll(searchitem, href = True, attrs = {'class':'_31qSD5'}):
-                    name=a.find('div', attrs = {'class':'h-display-flex'})
-                    price=a.find('div', attrs = {'class':'h-padding-r-tiny'})
-                    rating=a.find('div', attrs = {'class':'RatingStars__RatingStarsContainer-sc-k7ad82-2 lcJVIa'})
-                    products.append(name.text)
-                    prices.append(price.text)
-                    ratings.append(rating.text)
-                self.df3 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
-                self.df3.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
-            count += 1
-        return self.df1, self.df2, self.df3
-
     def displayItems(self, df1, df2, df3):
         self.item1.itemconfigure(window = df1)
         self.item2.itemconfigure(window = df2)
         self.item3.itemconfigure(window = df3)
+
+def webscraper(searchitem):
+    count = 0
+    #Adapted from example code - edureka.com/blog/web-scraping-with-python/
+    #Credit: Omkar S Hiremath
+    driver = webdriver.Chrome("D:\Program Files (x86)\ChromeDriver\chromedriver")
+    products=[] #List to store name of the product
+    prices=[] #List to store price of the product
+    ratings=[] #List to store rating of the product
+    while count <= 3:
+        if count == 1:
+            driver.get("https://www.amazon.com/s?k=" + searchitem)
+            content = driver.page_source
+            soup = BeautifulSoup(content)
+            for a in soup.findAll(searchitem, href = True, attrs = {'class':'s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16'}):
+                name=a.find('div', attrs = {'class':'a-section a-spacing-none puis-padding-right-small s-title-instructions-style'})
+                price=a.find('div', attrs = {'class':'a-section a-spacing-none a-spacing-top-micro s-price-instructions-style'})
+                rating=a.find('div', attrs = {'class':'a-section a-spacing-none a spacing-top-micro'})
+                products.append(name.text)
+                prices.append(price.text)
+                ratings.append(rating.text)
+            df1 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
+            df1.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
+        elif count == 2:
+            driver.get("https://www.walmart.com/search?q=" + searchitem)
+            content = driver.page_source
+            soup = BeautifulSoup(content)
+            for a in soup.findAll(searchitem, href = True, attrs = {'class':'mb1 ph1 pa0-xl bb b--near-white w-25'}):
+                name=a.find('div', attrs = {'class':'w_V_DM'})
+                price=a.find('div', attrs = {'class':'flex flex-wrap justify-start items-center lh-title mb2 mb1-m'})
+                rating=a.find('div', attrs = {'class':'flex items-center mt2'})
+                products.append(name.text)
+                prices.append(price.text)
+                ratings.append(rating.text)
+            df2 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
+            df2.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
+        else:
+            driver.get("https://www.target.com/s?searchTerm=" + searchitem)
+            content = driver.page_source
+            soup = BeautifulSoup(content)
+            for a in soup.findAll(searchitem, href = True, attrs = {'class':'Grid__StyledGrid-sc-1vq3yub0 bWTrMu'}):
+                name=a.find('div', attrs = {'class':'h-display-flex'})
+                price=a.find('div', attrs = {'class':'h-padding-r-tiny'})
+                rating=a.find('div', attrs = {'class':'RatingStars__RatingStarsContainer-sc-k7ad82-2 lcJVIa'})
+                products.append(name.text)
+                prices.append(price.text)
+                ratings.append(rating.text)
+            df3 = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings}) 
+            df3.to_csv('products.csv', index=False, encoding='utf-8') #For testing and debugging
+        count += 1
+    return df1, df2, df3
 
 def main():
     pct = PCT()
